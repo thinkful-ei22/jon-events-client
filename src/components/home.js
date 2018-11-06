96a723c8afb505280e28e265792edc76340b698f
Jonathan Kim <jonkim204@gmail.com>
2018-11-05 20:10:39 -0800

    added welcome message

--- src/components/home.js
+++ src/components/home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import SearchBox from './searchbox';
 import EventList from './eventlist';
 import Slideshow from './slideshow';
+import Welcome from './welcome';
 
 export default function Home(props) {
  
   return (
     <div>
+      <Welcome/>
       <Slideshow/>
       <SearchBox/>
       <EventList/>
---
+++ src/components/welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './welcome.css';
+
+export default function Welcome(props) {
+    return (
+        <div className="welcome-container">
+            <p className="welcome-message">Welcome.</p>
+            <p className="welcome-message">This is a place for those looking for a more intimate social gathering.</p>
+            <p className="welcome-message">A way for individuals to become a part of a community.</p>
+        </div>
+    )
+} 
